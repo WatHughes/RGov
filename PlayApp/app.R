3f350b0cbe2dcaee40e702e31f1778785f671811
Wat Hughes <wat.hughes@gmail.com>
2016-02-15 22:46:08 -0500

    Tabs, maps you can see, twiddles

--- PlayApp/app.R
+++ PlayApp/app.R
@@ -1,6 +1,7 @@
 require(shiny)
 require(leaflet)
 
+NumPoints = 100
 DataCenterLat = 37.5287955
 DataCenterLong = -77.493477
 
@@ -8,20 +9,53 @@ DataCenterLong = -77.493477
 # names(r_colors) <- colors()
 
 ui = fluidPage(
-    leafletOutput('mymap'),
-    p(),
-    actionButton('recalc', 'New points')
+    tabsetPanel
+    (
+        type='tabs',id='tabs'
+        ,tabPanel
+        (
+            'Documentation',value='Doc'
+            ,titlePanel('Doc')
+            ,h3('Application Documentation and Background')
+            ,br()
+            ,'This application '
+            ,br(),br()
+        ), # tabPanel - Documentation
+        tabPanel
+        (
+            'Random Data 1',value='Random'
+            ,br()
+            ,leafletOutput('mymap1',width = 800, height = 800)
+            ,p()
+            ,actionButton('recalc1', 'New points')
+        ), # tabPanel - Random 1
+        tabPanel
+        (
+            'Random Data 2',value='Random'
+            ,br()
+            ,leafletOutput('mymap2',width = 800, height = 800)
+            ,p()
+            ,actionButton('recalc2', 'New points')
+        ) # tabPanel - Random 2
+    ) # tabsetPanel
 ) # fluidPage
 
 server = function(input, output, session){
-    points = eventReactive(
-        input$recalc
+    points1 = eventReactive(
+        input$recalc1
         ,{
-            cbind(rnorm(40) / 10 * 2 + DataCenterLong, rnorm(40) / 20 + DataCenterLat)
+            cbind(rnorm(NumPoints) / 10 + DataCenterLong, rnorm(NumPoints) / 20 + DataCenterLat)
+        }
+        ,ignoreNULL=F)
+    points2 = eventReactive(
+        input$recalc2
+        ,{
+            cbind(rnorm(NumPoints) / 10 + DataCenterLong, rnorm(NumPoints) / 20 + DataCenterLat)
         }
         ,ignoreNULL=F)
 
-    output$mymap = renderLeaflet({
+
+    output$mymap1 = renderLeaflet({
         leaflet() %>%
             addProviderTiles(
 #                'Stamen.TonerLite'
@@ -29,7 +63,15 @@ server = function(input, output, session){
                 'MapQuestOpen.OSM'
                 ,options=providerTileOptions(noWrap=T)
         ) %>%
-            addMarkers(data=points())
+            addMarkers(data=points1())
+    })
+    output$mymap2 = renderLeaflet({
+        leaflet() %>%
+            addProviderTiles(
+                'OpenStreetMap.BlackAndWhite'
+                ,options=providerTileOptions(noWrap=T)
+        ) %>%
+            addMarkers(data=points2())
     })
 } # server
 
